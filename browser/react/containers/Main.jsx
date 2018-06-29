d20c694b141e73ef5653b77e50e348026e3c3bf6
Carola <Carola@MacBook-Pro-de-Santor.local>
2018-06-29 15:44:27 -0300

    componente search, falta terminar

--- browser/react/containers/Main.jsx
+++ browser/react/containers/Main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import { Login } from '../components/Loggin';
-import withStyles from '../components/Loggin'
+import Search from '../components/Search';
 
 
 
@@ -10,14 +8,44 @@ export default class Main extends React.Component {
     constructor() {
         super();
         this.state = {
+            search: "",
+            products: []
 
         }
+        this.setSearch = this.setSearch.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    setSearch(e) {
+        console.log(this.state.search)
+        this.setState({ search: e.target.value })
+
+
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        this.setState({ products: ["heladera 1", "heladera 2", "heladera 3"] })
+        console.log(this.state.products)
+        //     axios.get()
+        //         .then((result) => {
+
+        //             this.setState({
+        //                 products: [],
+        //                 search: ""
+        //             })
+        //             this.props.history.push('/search');
+        //         });
+    }
+
 
     render() {
         return (
             <div>
-                <Login />
+
+                <Search setSearch={this.setSearch} search={this.state.search} handleSubmit={this.handleSubmit} />
+
             </div>
         )
     }
