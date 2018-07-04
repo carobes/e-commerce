b9cb4b322ca951469c2d083ce961f6bca6ead0e8
Carlos Franchella <cfranche84@yahoo.com.ar>
2018-07-04 19:56:44 -0300

    Agregado Hash a password

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 const Users = models.Users;
+const crypto = require('crypto');
+const secret = 'Provoleta';
+
 
 module.exports = router;
 
 router.post('/new',function(req,res,next){
+    const hash = crypto.createHmac('sha256', secret)
+                   .update('I love cupcakes')
+                   .digest('hex');
+    req.body.password = hash
     Users.create(req.body)
     .then(data => res.status(201).json(data))
 })
