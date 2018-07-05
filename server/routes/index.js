6a10eed64e0354cc0625bf0a83d8ef1497809352
Dario <dvelezroman@gmail.com>
2018-07-05 16:39:39 -0300

    aplique direccionamiento hacia cart.js para las rutas /api/carrito, y en app.js le cambie en el console.log que muestre el error en caso que haya

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.use('/products', require('./products'));
 router.use('/users', require('./users'));
 router.use('/orders', require('./orders'));
+router.use('/carrito', require('./cart'));
 
 router.use(function (req, res) {
     res.status(404).end();
